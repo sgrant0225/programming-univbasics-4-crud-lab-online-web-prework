98fb0ad610d14926dc785ddb090d74692dca9f3a
Sharnell Grant <sharnellgrant@gmail.com>
2019-11-14 04:39:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,31 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  create_an_array = ["puzzles", "games", "books", "movies"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["pizza", "burgers", "cake", "candy"]
+  element = ["arrays!"]
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["rap", "country", "r&b", "soul"]
+  element = ["wow"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["blue", "red", "yellow", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow","duh", "hmph", "amazing"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
