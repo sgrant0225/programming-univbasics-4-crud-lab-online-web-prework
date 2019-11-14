3812dc3687f9d9cfffb797c990ad38ab43172cce
Sharnell Grant <sharnellgrant@gmail.com>
2019-11-14 04:58:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -47,6 +47,6 @@ def update_element_from_index(array, index_number, element)
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
     index_number = 4
     element = ["totally"]
-    array.push("totally")
+    array.push[4]("totally")
     
 end
