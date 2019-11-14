5db031f2a7d3fa4374ad7fb7e11f5c2490fde8b6
Sharnell Grant <sharnellgrant@gmail.com>
2019-11-14 04:48:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["blue", "red", "yellow", "arrays!"]
+  array[3]
 end
 
 def update_element_from_index(array, index_number, element)
